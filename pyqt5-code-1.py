dcf660a0a95161c1215a5c2ba77e970e258881b5
RITE  [ ПРОГРАМУВАННЯ ] <116145988+RITEUA@users.noreply.github.com>
2023-04-11 09:56:55 +0300

    Add files via upload
    Створення кнопки на Python + PyQt5, та додання методу, для зміни її тексту післня натискання на  неї

---
+++ pyqt5-code-1.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QPushButton
+from PyQt5.QtCore import QSize
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        # Налаштовуємо заголовок і розмір вікна програми
+        self.setWindowTitle("Кнопка")
+        self.setMinimumSize(QSize(200, 150))
+
+        # Створюємо і налаштовуємо кнопку
+        self.button = QPushButton("КЛАЦНИ", self)
+        self.button.resize(180, 130)
+        self.button.move(10, 10)
+        self.button.clicked.connect(self.click_button)
+
+    def click_button(self):
+        self.button.setText("ДЯКУЮ")
+
+
+# Запускаємо вікно програми
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
